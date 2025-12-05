d26ffeaf26eb07956859e98820b29f91d97e241d
Shekina-24 <shekinanguepi@gmail.com>
2025-12-05 21:56:01 +0100

    allow fron origins

--- app/main.py
+++ app/main.py
@@ -41,9 +41,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= "https://greencartfrontend-six.vercel.app",
+    allow_origins= ["https://greencartfrontend-six.vercel.app",],
     #allow_origins=settings.cors_origins,
-    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
