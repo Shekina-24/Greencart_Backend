2e6afbf2a6e7566cb5b968b580f38ddb7eee9650
Shekina-24 <shekinanguepi@gmail.com>
2025-12-06 10:22:46 +0100

    mise a jour bd.py

--- app/database.py
+++ app/database.py
@@ -49,6 +49,5 @@ async def init_db() -> None:
     # Ensure models are imported before creating tables.
     import app.models  # noqa: F401
 
-    if settings.debug:
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
